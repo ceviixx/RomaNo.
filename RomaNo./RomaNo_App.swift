6fe01e09416f15cfa8129ec3cf7f73b603256fd4
ceviixx <admin@ceviixx.io>
2025-07-08 17:46:34 +0200

    Initial Commit

---
+++ RomaNo./RomaNo_App.swift
@@ -0,0 +1,17 @@
+//
+//  RomaNo_App.swift
+//  RomaNo.
+//
+//  Created by Clemens Schäfer on 08.07.25.
+//
+
+import SwiftUI
+
+@main
+struct RomaNo_App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
